3d2457b574de77c26bc239494a171e1661258e95
Nibesh <nibesh@know-it.co.uk>
2023-02-27 14:52:34 +0000

    Commit 27/02

--- cypress/e2e/PageObjects/Check-itDashboard.cy.js
+++ cypress/e2e/PageObjects/Check-itDashboard.cy.js
@@ -40,25 +40,102 @@ class CheckitDashboard {
         return this
 
 }
-
         CreditReports() {
 
-        cy.xpath('//*[@id="archivedCreditReportsList"]/div/app-list/app-table/div/div[2]/div[2]/a')
+        cy.xpath('//*[@id="archivedCreditReportsList"]/div/app-list/app-table/div/div[2]/div[2]/a') .click ().should ("be.visible")
+        cy.wait(2000)
+        cy.go("back")
+        cy.log("Click on the company and click the back button to get to the dashboard")
+
+        cy.xpath('//*[@id="tabs"]/div/div[2]/ul/app-tab[1]/li') .click ()
+        .scrollTo ("bottom") .scrollTo ("top")
+        cy.log("Summary")
+
+        cy.xpath('//*[@id="tabs"]/div/div[2]/ul/app-tab[2]/li') .click () 
+        .scrollTo ("bottom") .scrollTo ("top")
+        cy.log("company profile")
+
+        cy.xpath('//*[@id="tabs"]/div/div[2]/ul/app-tab[3]/li') .click () 
+        .scrollTo ("bottom") .scrollTo ("top")
+        cy.log("Ratings")
+
+        cy.xpath('//*[@id="tabs"]/div/div[2]/ul/app-tab[4]/li') .click () 
+        .scrollTo ("bottom") .scrollTo ("top")
+        cy.log("financial")
 
         
+        cy.xpath('//*[@id="tabs"]/div/div[2]/ul/app-tab[5]/li') .click () 
+        .scrollTo ("bottom") .scrollTo ("top")
+        cy.log("Payment behaviour")
+
+        cy.xpath('//*[@id="tabs"]/div/div[2]/ul/app-tab[6]/li') .click () 
+        .scrollTo ("bottom") .scrollTo ("top")
+        cy.log("Company structure")
+
+        
+        cy.xpath('//*[@id="tabs"]/div/div[2]/ul/app-tab[7]/li') .click () 
+        .scrollTo ("bottom") .scrollTo ("top")
+        cy.log("Exceptional Events")
+
+        cy.xpath('//*[@id="printCreditReport"]') .click () 
+        .scrollTo ("bottom") .scrollTo ("top")
+        cy.log("Exceptional Events")
+
+        cy.xpath('//*[@id="container"]/app-navigation-bar/app-navigation-section[3]/ul/li[1]/a') .click ()
+        cy.wait(3000)
+        cy.log("Return back to the dashboard")
+
+        
+       cy.xpath('//*[@id="archivedCreditReportsList"]/div/app-button/button') .click ()
+       .should ("contain.text" , "View all")
+       .and("be.visible")
+        cy.wait("2000")
+        cy.go("back")
 
 
-
-
+       return this
+  
         }
-        
+
+     UnsecuredCreditors () {
+
+        cy.xpath('//*[@id="unsecuredCreditors"]/div/app-list/app-table/div/div[2]/div[2]/a').click ()
+        cy.wait(2000)
+        cy.go('back')
+        cy.xpath('//*[@id="unsecuredCreditors"]/div/app-button/button').click ()
+        cy.wait(2000)
+        cy.go('back')
+        cy.log("Usecured Creditors")
+
+        return this
+
+     }
+
+    GazetteNotices() {
+
+        cy.xpath('//*[@id="recentNoticesList"]/div/app-list/app-table/div/div[2]/div[6]/app-actions/ul/li/button') .click () .scrollTo ('bottom').scrollTo ('top')
+        cy.wait(2000)
+        cy.xpath('//*[@id="dashboard"]/div[3]/app-recent-notices/app-slidepanel/div[2]/div/div[1]/div/app-button/button').click ()
+        cy.log("close the pop up")
+
+        cy.xpath('//*[@id="recentNoticesList"]/div/app-list/app-table/div/app-paging/button[2]').click ()
+        cy.log("Click on the next button")
+
+        cy.xpath('//*[@id="recentNoticesList"]/div/app-button/button').click ()
+        cy.wait(2000)
+        cy.go('back')
+
+        return this
 
 
-        
 
-        
+
+
+    }
+           
 
 
     }
 
+    export default CheckitDashboard
 
