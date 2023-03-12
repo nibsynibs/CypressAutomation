e91982a8fd2747a4abe378936bae680fd493d40b
Nibesh <nibesh@know-it.co.uk>
2023-03-12 20:46:49 +0000

    commit  12.03.2023

--- cypress/e2e/PageObjects/FreePlan.cy.js
+++ cypress/e2e/PageObjects/FreePlan.cy.js
@@ -4,21 +4,24 @@ class SusbcriptionDowngrade {
 
     Createsubscription () {
 
-       cy.get("#createIcon") .click ()
-       cy.log ("Click on the + button")
+      cy.get("#createIcon") .click ()
+      cy.log ("Click on the + button")
 
-        cy.get("#createDropdown > div > ul > li").click
-        cy.log("Click Create Subscription")
+      cy.xpath("//li[@class='ng-tns-c274-2']").click ()
+      cy.log("Click Create Subscription")
+        
 
-        cy.get("#companynameLookup") .type("JP accountancy")
-        cy.wait(2000)
-        cy.get("#createcompanyForm > form > div.ng-star-inserted > app-lookup > div > div.results.ng-star-inserted.active > ul > li:nth-child(2) > p.companyName") .click ()
-        cy.get("#createcompanyForm > form > div.formButtons > app-button > button").click ()
-        cy.wait(3000)
-        cy.log("Select the first company from the search and then click get started")
-        cy.get("#confirmSubscriptionDetails") .click ()
-        cy.wait(10000)
-        cy.log("Click cnfirm")
+       cy.xpath("//input[@id='companynameLookup']") .click ()
+       cy.xpath("//input[@id='companynameLookup']") .type("JP Accountancy")
+       cy.wait(3000)
+       cy.xpath('//li[@class="companiesHouseResult ng-star-inserted"]').contains("07187190").click()
+       cy.wait(3000)
+       cy.xpath('//*[@id="createcompanyForm"]/form/div[2]/app-button/button') .click ()
+       cy.xpath('//*[@id="confirmSubscriptionDetails"]').click ()
+       cy.wait(10000)
+
+       
+
 
         return this
 
@@ -31,61 +34,87 @@ class SusbcriptionDowngrade {
         return false
       })
 
-            
-   cy. get ("#container > app-navigation-bar > app-navigation-section.my-integrations.ng-star-inserted > header > h2") .click () ;
-   cy. wait (3000) ;
-   cy. log ("Click on My integrations tab") ;
-    
-   cy.get("#integrationSectionHeader > div.buttons > app-button.ng-star-inserted > button").click () ;
-   cy.wait(3000) ;
-   cy.log ("click on Add Ledger") ;
+    cy. xpath ('//*[@id="container"]/app-navigation-bar/app-navigation-section[9]/header/h2') .click () ;
+    cy. wait (3000) ;
+    cy. log ("Click on My integrations tab") ;
+ 
 
-   cy.get ("#connectLedgerModal > div > div > div.accountsOptions > ul > li.xero > img") .click ()
-   cy.wait(3000)
-   cy.log("Click on Xero ledger")
+    cy.get("#integrationSectionHeader > div.buttons > app-button.ng-star-inserted > button").click () ;
+    cy.wait(3000) ;
+    cy.log ("click on Add Ledger") ;
 
+    cy.get("#connectLedgerModal > div > div > div.accountsOptions > ul > li.quickbooks > img").click () ;
+    cy.wait(5000)
+    cy.log ("connect quickbooks ledger") ; 
 
-   cy.origin('https://login.xero.com', () => {
-      
-   cy.get ("#xl-form-email") .type ("nibesh@know-it.co.uk")
-   cy.log("Enter xero username")
-   cy.get("#xl-form-password") .type ("Kit@123456")
-   cy.log("Enter xero password")
-   cy.get("#xl-form-submit") .click ();
-   cy.wait(5000)
-   cy.log("Login to xero")
-  })
+    cy.origin('https://accounts.intuit.com', () => {
 
+    cy.get ("#iux-username-password-sign-in-user-id-input").type ("nibesh@know-it.co.uk") ;
+    cy.get ("#iux-username-password-sign-in-password-input").type ("Kit@12345678") ;
+    cy.wait(3000)
+    cy.get("#ius-sign-in-widget > div > div > div.Bookends__NonStyledDiv-sc-163uul4-4.jdWbPG > div.styledComponents__StyledWidgetContainer-sc-12vb80e-14.lcEcRk.ius > div.UsernamePasswordSignIn__StyledUsernamePasswordSignInContainer-sc-9mm0lb-0.lBBBb > form > button") .click ()
+    cy.wait(10000)
+    cy.log("Enter QB username and password and click Sign in") ;
 
-   cy.origin("https://authorize.xero.com", function () {
-   cy.get ("#multipleTenants > div > button") .click()
-   cy.wait(3000)
-   cy.get("#\\37 7b0b224-624e-4fc7-9578-5bcc8a75fcc6 > button > span").click ()
-   cy.wait(2000)
+     }) ;
 
 
-   cy.get("#approveButton") .click ()
-   cy.wait(10000)
-   cy.log("Select ledger and allow access")
-  })
+    cy.origin('https://appcenter.intuit.com', () => {
 
-  return this
+    //  cy.get('#idsDropdownTypeaheadTextField2') .click ()
+    //  cy.wait(2000)
+     cy.get('#idsDropdownTypeaheadTextField2').type ("Statement test with small data").type ('{downArrow}') .type ('{enter}')
+     cy.wait(3000)
+
+
+       
+      // cy.get ("#app > div.shell-view > div > div > div.body > div > div > div > div > div.container-top > div > div:nth-child(3) > div > div > label > div > div > svg").click ()
+      // cy.wait(2000)
+      // cy.log("click on the search company dropdown")
+
+      // cy.get("#idsDropdownTypeahead1-item-6 > li > div > div").click ()
+      // cy.wait(2000)
+      // cy.log("select ledger")
+         
+           
+      cy.get ("#app > div.shell-view > div > div > div.body > div > div > div > div > div.container-footer > div > button.StyledButton__Wrapper-vnaxcc-1.gYvrJh.pull-right.btn-next").click ()
+      cy.wait(3000)
+      cy.log ("Click on the next button")
+
+      cy.get("#app > div.shell-view > div > div > div.body > div > div > div > div > div.authorize-actions-visual-update > form > button").click ()
+      cy.wait(10000)
+      cy.log("click on the connect button")
+
+      cy.wait(300000)
+   
+       }) ;
+
+
+       return this        
+ 
 
 }
 
   CheckitDashboard () {
 
+    cy.on('uncaught:exception' ,() => {
+
+      return false
+
+
+    })
+
     cy.get("#container > app-navigation-bar > app-navigation-section.check.ng-star-inserted > header > img"). click ()
     cy.get("#container > app-navigation-bar > app-navigation-section.check.ng-star-inserted > ul > li:nth-child(1) > a") .click ()
-    Cypress.log("Click on Check-it Dashboard")
+    cy.log("Click on Check-it Dashboard")
 
     cy.get("#creditReportMonitoringList > div > app-list > app-table > div > div:nth-child(2) > div.field_change.flex-row.ng-star-inserted > div > div > a") .click ()
     cy.wait(3000)
-    cy.log("Click on Purchase to vide on the Credit Monitoring Section")
+    cy.log("Click on Purchase to view on the Credit Monitoring Section")
 
-    cy.go('back')
-    cy.wait(3000)
-    cy.log("click to go back")
+    cy.get("#container > app-navigation-bar > app-navigation-section.check.ng-star-inserted > header > img"). click ()
+    cy.get("#container > app-navigation-bar > app-navigation-section.check.ng-star-inserted > ul > li:nth-child(1) > a") .click ()
+    cy.log("Click on Check-it Dashboard")
 
    cy.get("#creditReportMonitoringList > div > app-list > app-table > div > div:nth-child(2) > div.field_actions.flex-row.ng-star-inserted > app-actions > ul > li:nth-child(2) > app-button > button") .click ()
    cy.wait(3000)
@@ -118,4 +147,3 @@ export default SusbcriptionDowngrade
 
 
 
-}
--- cypress/e2e/TestCases/CollectCases.cy.js
+++ cypress/e2e/TestCases/CollectCases.cy.js
@@ -1,11 +1,12 @@
 ///<reference types = "cypress"/>
 
-import  collectitCase from "../PageObjects/CollectitCases"
+import  collectitCase from "../PageObjects/CollectitCases.cy"
 
 describe ("collect-it cases" , function () {
     it ("cases" , function (){
 
     cy.login ()
+   
     const cs = new collectitCase ()
     cs.cases (" DIISR - Small Business Services")
     cy.logout ()
